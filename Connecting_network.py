c855728935c45618f6cbcac2dca7380258c53bfc
gouwon <jskd2938@gmail.com>
2019-12-18 10:12:26 +0900

    191218 connect wifi v1.1 complete

--- Connecting_network.py
+++ Connecting_network.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 
 def do_connect():
-    print(".............. SK 와이파이에 접속 중입니다.")
+    print(".................. SK 와이파이에 접속 중입니다.")
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     options.add_argument("disable-gpu")
@@ -11,36 +11,30 @@ def do_connect():
     driver = webdriver.Chrome("chromedriver")
     driver.get('https://www.tfreewifi.com/wifi.do/')
     time.sleep(5)
-    
-    print(".............. SK 와이파이로부터 인증 중입니다.")
+    print(".................. SK 와이파이로부터 인증 중입니다.")
     try:
+        driver.implicitly_wait(4)
         driver.find_element_by_class_name('btn__confirm').click()
-        time.sleep(3)
-        print(".............. SK 와이파이로부터 인증이 완료되었습니다.")
+        time.sleep(5)
+        print(".................. SK 와이파이로부터 인증이 완료되었습니다.")
     except Exception as drivererr:
-        print(".............. 셀레니움 크롬드라이버(Chromedriver)에서 오류가 발생하였습니다.")
+        print(".................. 셀레니움 크롬드라이버(Chromedriver)에서 오류가 발생하였습니다.")
         print(">>>>>>>> drivererr message ", drivererr)
+    finally:
+        driver.quit()
     
-    time.sleep(8)
+    time.sleep(5)
     driver.quit()
 
 def check_connect():
-    print(".............. 구글(https://www.google.com/)에 요청을 보내 연결을 확인합니다.")
     try:
-        req = requests.get('https://www.google.com/', timeout=(10, 10))
-        print(".............. 인터넷 연결에 문제가 없습니다.")
+        print(".................. 인터넷 연결 프로그램을 시작합니다.")
+        do_connect()
     except Exception as err:
-        print(".............. 인터넷 연결에 오류가 발생하였습니다.")
+        print(".................. 인터넷 연결 도중 오류가 발생하였습니다.")
         print(">>>>>>>> error message ", err)
-        try:
-            do_connect()
-        except Exception as second_err:
-            print(".............. 인터넷 연결 도중 오류가 발생하였습니다.")
-            print(">>>>>>>> error message ", second_err)
-            pass
-    
-    time.sleep(1)
-    print(".............. 인터넷 연결 프로그램을 종료합니다. ")
-
+    finally:
+        time.sleep(1)
+        print(".................. 인터넷 연결 프로그램을 종료합니다.")
 
 check_connect()
--- check_network_connection.sh
+++ check_network_connection.sh
@@ -1,34 +1,103 @@
 #!/bin/sh
 
-echo $0, $1
+echo "프로그램이 종료되는 시간이 $1 으로 지정되었습니다."
 
 dueTime=$1
-
-server="210.220.163.82" ## SK
+server="google.com"
 valid=1
+route_connect
 
-while ((${valid}==1));
-do
-    ping -c 1 | awk '{result=$2} END {print result}'
-    if [ $result=="timeout" ];
+# Airport command is private so make instance airport command using function.
+function airport(){
+    AIRPORT_PATH="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport "
+    AIRPORT_COMMAND=${AIRPORT_PATH}${1}
+    ${AIRPORT_COMMAND}
+}
+
+# Check mac's WIFI Router on/off and wifi router connection.
+function do_wifi(){
+    status=$(ifconfig en5 | grep status | awk '{print $2}')
+    echo "현재 기기의 WIFI 상태는 ${status}입니다."
+
+    if [ "${status}" != "active" ];
     then
-        echo "$server ..... 네트워크 연결에 문제가 있습니다."
-        python ~/workspace/GW_Study/Connecting_network.py
+        echo "기기의 WIFI가 꺼져 있습니다."
+        echo "기기의 무선 네트워크 랜 카드를 작동합니다."
+        sudo -p " " ifconfig en5 up
+
+        airport -s | awk '{print $1}' | grep "T Free WiFi Zone"
+
+        if [ $? == 0 ];
+        then
+            echo "주변에서 T Free WiFi Zone을 검색하였습니다."
+            echo "해당 WIFI에 연결합니다....."
+            networksetup -setairportnetwork en5 "T Free WiFi Zone"
+
+            route_connect=$(airport -I | grep state | awk '{print $2}')
+            SSID=$(airport -I | grep SSID | awk '{print $2}')
+
+            if [ "${route_connect}" == "running" ];
+            then
+                echo "${SSID} 에 연결되었습니다."
+            else
+                echo "WIFI 라우터 연결에 실패하였습니다."
+            fi
+        else
+            echo "등록된 WIFI가 없습니다."
+        fi
     else
-        echo "$server ..... 네트워크 연결이 정상입니다."
+        echo "기기의 WIFI가 켜져 있습니다."
+        route_connect=$(airport -I | grep state | awk '{print $2}')
+    fi
+}
+
+# main flow
+# first check route connect of mac. if failed, connect to wifi route until connected.
+# then do ping test to google.com
+# if failed, try to connect to wifi, until connected.
+# while-loop is infinite loop, it makes wifi connection until dueTime.
+# => # First check your Mac's path connection. If it fails, connect to the wifi path until you are connected.
+# Ping test on google.com
+# If it fails, try to connect to wifi until you are connected.
+# while-loop is an infinite loop, so create a wifi connection until dueTime
+while ((${valid}==1));
+do  
+    until [ "${route_connect}" == "running" ];
+    do
+        do_wifi
+    done
+
+    echo "................... PING TEST를 진행합니다."
+    echo "................... 구글(https://www.google.com/)에 요청을 보내 연결을 확인합니다."
+    ping -c 3 -W 1 ${server}
+    result=$?
+    echo "................... 결과는 ${result} 입니다."
+
+    if [ "${result}" == 0 ];
+    then
+        echo "${server} ........ 네트워크 연결이 정상입니다."
+    else
+        until [ "${result}" == 0 ];
+        do
+            echo "${server} ........ 네트워크 연결에 문제가 있습니다."
+            python ~/workspace/GW_Study/Connecting_network.py
+            ping -c 3 -W 1 ${server}
+            result=$?
+        done
+        echo "${server} ........ 네트워크에 연결되었습니다."
     fi
 
     local_currentTime=$(date +%H:%M)
     _dueTime=${dueTime:0:2}
-    _currentTime=$(date +%H)
 
-    if (($_dueTime == $_currentTime));then
-        echo "지정된 시간 ${dueTime} 이 지났습니다. 프로그램을 종료합니다."
+    if [ ${_dueTime} == ${local_currentTime:0:2} ];
+    then
+        echo ">> 지정된 시간 ${dueTime} 이 지났습니다. 프로그램을 종료합니다."
         valid=0
         sleep 3;
     else
-        echo "현재 시각은 ${local_currentTime} 입니다."
-        sleep 120;
+        echo ">> 현재 시각은 ${local_currentTime} 입니다."
+        sleep 600;
     fi
 done
 
